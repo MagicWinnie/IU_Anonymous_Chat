d89b45adeaa89b815c85f65e419f50c3c8022ea6
Dmitriy Okoneshnikov <magicwinnie@yandex.ru>
2024-09-18 13:22:41 +0300

    Moved out texts

--- client/main.py
+++ client/main.py
@@ -1,5 +1,6 @@
 import flet as ft
 
+import texts
 from message_row import ChatMessage
 from schemas import Message
 from utils import is_valid_url
@@ -7,20 +8,20 @@ from utils import is_valid_url
 
 def main(page: ft.Page):
     page.horizontal_alignment = ft.CrossAxisAlignment.STRETCH
-    page.title = "Anonymous Chat"
+    page.title = texts.PAGE_TITLE
 
     def join_chat_click(e):
         if not ip_address_server.value:
-            ip_address_server.error_text = "Server address cannot be blank!"
+            ip_address_server.error_text = texts.SERVER_ADDRESS_EMPTY
             ip_address_server.update()
         elif not is_valid_url(ip_address_server.value):
-            ip_address_server.error_text = "Server address is invalid!"
+            ip_address_server.error_text = texts.SERVER_ADDRESS_INVALID
             ip_address_server.update()
         else:
             page.close(dig)
 
     def send_message_click(e):
-        if new_message.value != "":
+        if new_message.value:
             page.pubsub.send_all(Message(text=new_message.value))
             new_message.value = ""
             new_message.focus()
@@ -33,7 +34,7 @@ def main(page: ft.Page):
     page.pubsub.subscribe(on_message)
 
     ip_address_server = ft.TextField(
-        label="Enter the address of the server",
+        label=texts.SERVER_ADDRESS_INPUT_LABEL,
         autofocus=True,
         on_submit=join_chat_click,
     )
@@ -41,9 +42,9 @@ def main(page: ft.Page):
     dig = ft.AlertDialog(
         open=True,
         modal=True,
-        title=ft.Text("Welcome!"),
+        title=ft.Text(texts.WELCOME_DIALOG_TITLE),
         content=ft.Column([ip_address_server], width=300, height=70, tight=True),
-        actions=[ft.ElevatedButton(text="Join anonymous chat", on_click=join_chat_click)],
+        actions=[ft.ElevatedButton(text=texts.WELCOME_DIALOG_JOIN_BUTTON, on_click=join_chat_click)],
         actions_alignment=ft.MainAxisAlignment.END,
     )
     page.overlay.append(dig)
@@ -53,7 +54,7 @@ def main(page: ft.Page):
 
     # A new message entry form
     new_message = ft.TextField(
-        hint_text="Write a message...",
+        hint_text=texts.NEW_MESSAGE_INPUT_HINT,
         autofocus=True,
         shift_enter=True,
         min_lines=1,
@@ -75,7 +76,11 @@ def main(page: ft.Page):
         ft.Row(
             [
                 new_message,
-                ft.IconButton(icon=ft.icons.SEND_ROUNDED, tooltip="Send message", on_click=send_message_click),
+                ft.IconButton(
+                    icon=ft.icons.SEND_ROUNDED,
+                    tooltip=texts.SEND_MESSAGE_TOOLTIP,
+                    on_click=send_message_click
+                ),
             ]
         ),
     )
---
+++ client/texts.py
@@ -0,0 +1,12 @@
+PAGE_TITLE = "Anonymous Chat"
+
+SERVER_ADDRESS_EMPTY = "Server address cannot be blank!"
+SERVER_ADDRESS_INVALID = "Server address is invalid!"
+SERVER_ADDRESS_INPUT_LABEL = "Enter the address of the server"
+
+WELCOME_DIALOG_TITLE = "Welcome!"
+WELCOME_DIALOG_JOIN_BUTTON = "Join anonymous chat"
+
+NEW_MESSAGE_INPUT_HINT = "Write a message..."
+
+SEND_MESSAGE_TOOLTIP = "Send message"
